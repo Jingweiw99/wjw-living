e9441740e36d421a235b5175fc9f9d414052fcec
wjw <Wangjingwei0@foxmail.com>
2024-06-09 12:20:02 +0800

    初始化后端项目
    添加脚手架创建系统管理，添加商品模块。前后端联调成功

---
+++ wjwliving-commodity/src/main/java/com/wjw/wjwliving/wjwlivingcommodity/WjwlivingCommodityApplication.java
@@ -0,0 +1,13 @@
+package com.wjw.wjwliving.wjwlivingcommodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjwlivingCommodityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjwlivingCommodityApplication.class, args);
+    }
+
+}
---
+++ wjwliving-commodity/src/test/java/com/wjw/wjwliving/wjwlivingcommodity/WjwlivingCommodityApplicationTests.java
@@ -0,0 +1,11 @@
+package com.wjw.wjwliving.wjwlivingcommodity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjwlivingCommodityApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
