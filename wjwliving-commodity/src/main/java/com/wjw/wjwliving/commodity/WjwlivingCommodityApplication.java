c5762d914991a900eb62363d0e628605fe086399
wjw <Wangjingwei0@foxmail.com>
2024-06-10 13:48:53 +0800

    家具信息展示

---
+++ wjwliving-commodity/src/main/java/com/wjw/wjwliving/commodity/WjwlivingCommodityApplication.java
@@ -0,0 +1,11 @@
+package com.wjw.wjwliving.commodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjwlivingCommodityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WjwlivingCommodityApplication.class, args);
+    }
+}
