3480704c44b2ded7e5e45db5b21b10b2386c86b0
wjw <Wangjingwei0@foxmail.com>
2024-06-13 14:55:28 +0800

    项目集成第三方服务oss
    1. 阿里云oss创建Bucket，创建子账户RAM,并拿到   access-key，secret-key，endpoint: oss-cn-beijing.aliyuncs.com，bucket
    1.2. 保证能够上传文件
    2. 创建第三方服务模块wjwliving-service，项目添加spring-cloud-starter-alicloud-oss
    3. 添加前端上传组件，修改之前的上传，发送请求。
    3.1. 会出现跨域问题，前端到后端wjwliving-service微服务跨域了，CorsConfig解决
    3.2. alioss配置跨域
    4. 前端上传的logo展示
    5. 注掉敏感信息，不上传远程

---
+++ wjwliving-service/src/main/java/com/wjw/wjwliving/service/WjwlivingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.wjw.wjwliving.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjwlivingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WjwlivingServiceApplication.class, args);
+    }
+}
