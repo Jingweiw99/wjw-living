84816cdd84e3e947a91490143d5ba2e662608ef3
wjw <Wangjingwei0@foxmail.com>
2024-06-15 15:02:22 +0800

    集成Gateway，统一网关请求、统一处理跨域

--- wjwliving-service/src/main/java/com/wjw/wjwliving/service/config/CorsConfig.java
+++ wjwliving-service/src/main/java/com/wjw/wjwliving/service/config/CorsConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .maxAge(3600);
+        // gateway统一跨域
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .maxAge(3600);
     }
 }
