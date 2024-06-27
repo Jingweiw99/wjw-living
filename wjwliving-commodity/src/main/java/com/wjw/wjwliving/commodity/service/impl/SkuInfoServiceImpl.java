b5e70ca9a356248de2351f9e1443d7f63dc7cfba
wjw <Wangjingwei0@foxmail.com>
2024-06-27 16:34:59 +0800

    完成商品sku管理

--- wjwliving-commodity/src/main/java/com/wjw/wjwliving/commodity/service/impl/SkuInfoServiceImpl.java
+++ wjwliving-commodity/src/main/java/com/wjw/wjwliving/commodity/service/impl/SkuInfoServiceImpl.java
@@ -2,8 +2,12 @@ package com.wjw.wjwliving.commodity.service.impl;
 
 import com.wjw.utils.PageUtils;
 import com.wjw.utils.Query;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -30,5 +34,50 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
     }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+        //带上查询条件
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and((wrapper) -> {
+                wrapper.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+        //带上分类
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId) && !"0".equalsIgnoreCase(catelogId)) {
+            queryWrapper.eq("catalog_id", catelogId);
+        }
+        //带上品牌
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+
+        //带上价格范围
+        String min = (String) params.get("min");
+        if (!StringUtils.isEmpty(min)) {
+            queryWrapper.ge("price", min);
+        }
+        // max 有值,并且大于 0
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(max)) {
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if (bigDecimal.compareTo(new BigDecimal("0")) == 1) {
+                    queryWrapper.le("price", max);
+                }
+            } catch (Exception e) {
+            }
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params), queryWrapper
+        );
+        return new PageUtils(page);
+    }
+
 
 }
